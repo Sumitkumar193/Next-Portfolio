6b98ee770d0ad6a9386a0a01b0cff2e0e84d89c4
Sumit Kumar <sumit.kumar@hestabit.in>
2024-07-16 00:19:53 +0530

    first commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,4 @@
-import type { Config } from "tailwindcss"
-
-const config = {
+module.exports = {
   darkMode: ["class"],
   content: [
     './pages/**/*.{ts,tsx}',
@@ -8,7 +6,6 @@ const config = {
     './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
 	],
-  prefix: "",
   theme: {
     container: {
       center: true,
@@ -60,12 +57,12 @@ const config = {
       },
       keyframes: {
         "accordion-down": {
-          from: { height: "0" },
+          from: { height: 0 },
           to: { height: "var(--radix-accordion-content-height)" },
         },
         "accordion-up": {
           from: { height: "var(--radix-accordion-content-height)" },
-          to: { height: "0" },
+          to: { height: 0 },
         },
       },
       animation: {
@@ -75,6 +72,4 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
-
-export default config
+}
