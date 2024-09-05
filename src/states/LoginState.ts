b0fa485a4954f3185901ff43862a9fd2d88cf483
Sumit Kumar <itsme.sumit96@gmail.com>
2024-09-05 07:07:13 +0000

    types added to loginstate

--- src/states/LoginState.ts
+++ src/states/LoginState.ts
@@ -1,12 +1,23 @@
 import { atom, selector } from "recoil";
 
-const getProfileFromCookies = () => {
-    if (typeof window === "undefined") return null;
-    const cookies = document.cookie.split("; ");
-    const profileCookie = cookies.find((cookie) => cookie.startsWith("profile="));
-    if (!profileCookie) return null;
-    const profile = JSON.parse(atob(profileCookie.split("=")[1]));
-    return profile;
+type Profile = {
+    id?: string;
+    name: string;
+    email: string;
+};
+
+function getProfileFromCookies(): Profile | null {
+    try {
+        if (typeof window === "undefined") throw new Error("No window object");
+        const cookies = document.cookie.split("; ");
+        const profileCookie = cookies.find((cookie) => cookie.startsWith("profile="));
+        if (!profileCookie) throw new Error("No profile cookie");
+        const profile: Profile = JSON.parse(atob(profileCookie.split("=")[1]));
+        return profile;
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 
 const ProfileState = atom({
@@ -18,7 +29,7 @@ export const isLoggedInSelector = selector({
     key: "isLoggedInSelector",
     get: ({ get }) => {
         const profile = get(ProfileState);
-        return !!profile?.email;
+        return !!profile;
     },
 });
 
